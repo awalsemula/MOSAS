f5cf5fc3890d9d0b004a5789fa28117a63e3db34
awalsemula <m.mustamiin@gmail.com>
2019-07-14 14:53:33 +0700

    Initial commit

---
+++ app/src/main/java/com/proyek1/mstasys/response/Semester.java
@@ -0,0 +1,32 @@
+package com.proyek1.mstasys.response;
+
+public class Semester {
+    private String id_semester;
+    private String semester;
+
+    public String getThn_ajaran() {
+        return thn_ajaran;
+    }
+
+    public void setThn_ajaran(String thn_ajaran) {
+        this.thn_ajaran = thn_ajaran;
+    }
+
+    private String thn_ajaran;
+
+    public String getId_semester() {
+        return id_semester;
+    }
+
+    public void setId_semester(String id_semester) {
+        this.id_semester = id_semester;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+}
